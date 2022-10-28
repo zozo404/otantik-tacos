c09c1d9240e27bce2b8cd6dea676ac1a993b263b
zozo404 <enzocuny66@gmail.com>
2022-10-28 11:21:29 +0200

    ✨ | component RetrouvezNous with iframe for map

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <PresentationTacos class="pt-6" />
     <NotreCarte />
-    <Titles :id="'retrouvez'" subtitle="Retrouvez-nous" class="scroll-mt-60 lg:scroll-mt-40" />
+    <RetrouvezNous />
     <Titles :id="'horaires'" class="scroll-mt-60 lg:scroll-mt-40" subtitle="Nos horaires" />
   </div>
 </template>
